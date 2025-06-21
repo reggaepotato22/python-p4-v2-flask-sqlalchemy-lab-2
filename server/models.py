45c8df17525128a9d638229cf07f6b186a8ef45b
Andrew <andrewmandieka@gmail.com>
2025-06-22 00:39:36 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
-
+from sqlalchemy import MetaData # Import MetaData
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -10,23 +9,48 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', backref='customer', cascade='all, delete-orphan')
+    items = association_proxy('reviews', 'item')
+
+    serialize_rules = ('-reviews.customer',)
+
     def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+        return f'<Customer {self.id}: {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', backref='item', cascade='all, delete-orphan')
+    customers = association_proxy('reviews', 'customer')
+
+    serialize_rules = ('-reviews.item',)
+
     def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+        return f'<Item {self.id}: {self.name}, ${self.price}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    
+    # Foreign keys
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}: {self.comment}>'
